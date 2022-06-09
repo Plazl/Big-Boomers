bb13e166cff73897e375c1ea23d80056a9f8fb27
jhnbg rth <I1234itsplaz@outlook.com>
2022-06-09 12:55:18 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+bombs = {};
+bombYeild = {};
+population = 8000000000;
+upgrades = {};
+var game = {
+ "bombs":bombs,
+ "bombYeild":bombYeild,
+ "population":population,
+ "upgrades":upgrades
+}
+delete bombs;
+delete bombYeild;
+delete population;
+function dropBomb() {
+ 
+}
