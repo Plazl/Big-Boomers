75e4876ed0a0c3e85eea7829a5d7648eb2164a47
jhnbg rth <I1234itsplaz@outlook.com>
2022-06-13 09:07:52 -0400

    Update core.js

--- core.js
+++ core.js
@@ -1,16 +1,27 @@
 bombs = {};
 bombYeild = {};
 population = 8000000000;
+contPop = {
+"NA":579000000,
+"SA":422500000,
+"Asia":45610000,
+"Europe":746400000,
+"Africa":1216000000
+};
+countPop = {};
 upgrades = {};
+deaths;
 var game = {
  "bombs":bombs,
  "bombYeild":bombYeild,
  "population":population,
- "upgrades":upgrades
+ "upgrades":upgrades,
+ "deaths":deaths
 }
 delete bombs;
 delete bombYeild;
 delete population;
+delete deaths;
 function dropBomb() {
  
 }
